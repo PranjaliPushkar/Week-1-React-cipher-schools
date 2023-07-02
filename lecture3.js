ccf7464325a0c8382489614852bd916e3763f53a
PranjaliPushkar <75657786+PranjaliPushkar@users.noreply.github.com>
2023-07-02 18:04:29 +0530

    lecture3.js

--- lecture3.js
+++ lecture3.js
@@ -19,18 +19,89 @@ class TestClass{
         if(!this.hasLast()){
             return null;
         }
+
         let current = this.head;
         while(current.next != null){
             current = current.next;
         }
+
         let finalValue = current.val;
         current = this.head;
-        while(current.next.next !== null){
+        while(current.next != null&&current.next.next !== null){
             current = current.next;
         }
-        current.next.next = null
+        if(current.next ===null){
+            this.head = null;
+        }
+        current.next = null
         return finalValue;
 
     }
 
 }
+
+let  myVariable = new TestClass(
+    new ListNode(
+        1,
+        new ListNode(2, new ListNode(3,new ListNode(4, new ListNode (5))))
+    )
+);
+console.log(myVariable.getLast());
+console.log(myVariable.getLast());
+console.log(myVariable.getLast());
+console.log(myVariable.getLast());
+console.log(myVariable.getLast());
+
+//promise
+//promise is a class in JS
+//constructor (function)
+
+let promise = new Promise((fulfilled, notFulfilled) =>{
+    let num1 = 10;
+    let num2 = 15;
+    let sum = num1 + num2;
+    if (sum > 24){
+    fulfilled(sum);
+    }
+    else {
+        notFulfilled(new Error("Promise not fulfilled"));
+    }   
+});
+
+myPromise
+    .then(() => {
+        console.log("Promis was fulfilled");
+    })
+    .catch(() => {
+        console.log("Promis was not Fulfilled");
+    });
+
+
+    function addTwoNumbers(num1, num2) {
+        let sum = 0;
+        setTimeout(() => {
+            console.log(num1, num2);
+        }, 5000);
+        console.log("Inside addTwoNumbers");
+        return sum;
+    }
+    console.log(addTwoNumbers(2,3));
+
+    function addTwoNumbers(num1, num2) {
+        return new Promise((fulfilled, notFulfilled) => {
+            setTimeout(() => {
+                console.log("Inside setTimeout");
+                    fulfilled(num1 + num2);
+            }, 5000);
+            notFulfilled(new Error("An Error occurred!");)
+        });
+    }
+    addTwoNumbers(10,5) (
+        .then() => {
+            console.log("Promise was filfilled");
+        });
+        .catch(() => {
+            console.log("Promise was notFilfilled");
+        });
+
+
