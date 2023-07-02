56d360c2b0b17e3e90322e669e65a2adaefa0402
PranjaliPushkar <75657786+PranjaliPushkar@users.noreply.github.com>
2023-07-02 17:57:58 +0530

    lecture2.js

--- lecture2.js
+++ lecture2.js
@@ -68,6 +68,74 @@ return el1-el2;
 });
 console.log(sortArray);
 
+/Object Destructuring
+let details = {
+    name: "John",
+    age: "24",
+    address: {
+        city: "New York",
+        state: "NY",
+        country: "USA",
+        street: "Brooklyn",
+        passportDetails: {
+            passportNumber: "ABCD1234",
+        },
+    },
+};
+console.log(details.address.passportDetails.passportNumber);
+
+//Array matching and Object matching
+let obj1 = {name: "Ram"};
+let obj2 = {name: "Ram"};
+functionareEqual(obj1,obj2) {
+    if (obj1 == obj2) {
+        return true;
+    }
+    if(object.key(obj1).length ! = object.key(obj2).length) {
+        return false;
+    }
+    for (let key of object.keys(obj1)) {
+        if (typeof obj2[key] == "undefined") {
+            return false;
+        }
+        if (obj2[key] != obj1[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(areEqual(obj1, obj2));
+
+//Map ans Set
+let map = new map();
+map.set(1, "Ram");
+map.set2(2, "Alex");
+console.log(map);
+map.value()
+
+let set = new set();
+set.add(1);
+set.add(2);
+console.log(set);
+
+//Class
+class Animal {
+    noOfLegs;
+    color;
+    family;
+    sound;
+    constructor(noOfLegs.color,family,sound) {
+        this.noOfLegs = noOfLegs;
+        this.color = color;
+        this.family = family;
+        this.sound = sound;
+    }
+}
+let animal = new Animal(4, "browm","rodent","something");
+console.log(animal);
+
+
+
 
 
 
